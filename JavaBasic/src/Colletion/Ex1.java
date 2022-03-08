ce1ff6eeca6612bc63f22c1cd35abb24901bd763
ehs2803 <hr2803@naver.com>
2022-03-08 09:43:19 +0900

    20220308

---
+++ JavaBasic/src/Colletion/Ex1.java
@@ -0,0 +1,13 @@
+package Colletion;
+
+import java.util.ArrayList;
+
+public class Ex1 {
+    public static void main(String[] args){
+        ArrayList list1 = new ArrayList(10);
+        list1.add(new Integer(1));
+
+
+    }
+}
+
